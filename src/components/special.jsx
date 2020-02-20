5cbeeb63d7d0879c52aa7c7a30d3df0b215d3467
Numbericons <zachjoliver@gmail.com>
2020-02-19 23:16:32 -0800

    Change cooldown text to show if ready

--- src/components/special.jsx
+++ src/components/special.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function Special(props) {
+  const cdText = props.special[3] === 0 ? 'Ready!' : `Cooldown: ${ props.special[3]} /${props.special[2]}`;
   return(
     <div>
-      {`${props.special[1]} Damage: ${props.special[4]} Cooldown: ${props.special[3]} /${props.special[2]}`}
+      {`${props.special[1]} Damage: ${props.special[4]} ${cdText}`}
     </div>
   );
 }
